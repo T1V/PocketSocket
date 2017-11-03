3ba5ecbb8df4fdbaf033d8d2c40738c7ef86c997
Adam Fedor <afedor@t1v.com>
2017-11-03 09:26:03 -0600

    Switch Mac target to a Framework

---
+++ PocketSocket-Mac/PocketSocket.h
@@ -0,0 +1,12 @@
+//
+//  PocketSocket
+//
+//  Copyright © 2017 Zwopple Limited. All rights reserved.
+//
+
+#import <PocketSocket/PSWebSocket.h>
+#import <PocketSocket/PSWebSocketServer.h>
+#import <PocketSocket/PSWebSocketDriver.h>
+#import <PocketSocket/PSWebSocketTypes.h>
+
+
